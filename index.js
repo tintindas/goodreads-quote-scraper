df8026cd3d7b0f504d9b1ccd3418960abe24beec
Upamanyu Das <47525983+tintindas@users.noreply.github.com>
2020-03-06 17:10:47 +0530

    add comments

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const request = require('request');
 const cheerio = require('cheerio');
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/quotesDB", {
+mongoose.connect("mongodb://localhost:27017/quotesDB", {   //connect to local or cloud database
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
+
 const Schema = mongoose.Schema;
 
 const quoteSchema = new Schema({
@@ -19,7 +20,7 @@ const Quote = new mongoose.model("Quote", quoteSchema);
 for (let i = 1; i <= 5; i++) {
   //Navigate to Goodreads quotes page to get base URL, copy and paste here
   const baseURL = "https://www.goodreads.com/author/quotes/1654.Terry_Pratchett";
-  const authorName = "Terry Pratchett"
+  const authorName = "Terry Pratchett" //replace with author name as given in above URL
   let url = baseURL + "?page=" + String(i);
 
   request(url, function(err, res, body) {
@@ -36,18 +37,18 @@ for (let i = 1; i <= 5; i++) {
     quoteElements.forEach(element => {
       let quoteText = element.substr(0, element.indexOf("―"));
       quoteText = quoteText.trim();
-      const offset = authorName.length + 11; // Offset calculated as length of author name
+      const offset = authorName.length + 11; // Offset calculated as length of author name + 11 
       let quoteSource = element.substr(element.indexOf("―") + offset);
       quoteSource = quoteSource.trim();
 
 
       const quote = new Quote({
         text: quoteText,
-        author: authorName, // Replace with author Name
-        source: quoteSource //Replace with source
+        author: authorName, 
+        source: quoteSource 
       });
 
-      quote.save();
+      quote.save();   //saves quote to database
     });
 
   });
